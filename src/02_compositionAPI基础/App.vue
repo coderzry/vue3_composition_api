ee68adb9a5e1082011372c07e163d2002a64027a
coderzry <18348738308@163.com>
2021-10-03 21:59:59 +0800

    props、context参数的使用

---
+++ src/02_compositionAPI基础/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <home message="哈哈" id="aaa" class="bbb"/>
+  </div>
+</template>
+
+<script>
+import Home from './Home.vue'
+export default {
+  components: {
+    Home
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/02_compositionAPI基础/Home.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <h2>Home Page</h2>
+    {{ message }}
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    message: {
+      type: String,
+      required: true
+    }
+  },
+  /*
+    参数一： props，父组件传递过来的属性 
+  */
+  // setup函数有哪些参数？
+  // setup函数有什么样的返回值
+  // context写法
+  // setup(props, context) {
+  // 对context解构的写法， 可以用到哪个写哪个
+  setup(props, {attrs, slots, emit}) {
+    console.log(props.message);
+    console.log(attrs.id, attrs.class);
+    console.log(slots);
+    console.log(emit);
+  }
+}
+</script>
+
+<style>
+
+</style>
