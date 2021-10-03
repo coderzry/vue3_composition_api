200f8c8fdfd8d025dc315abd89dc033d9d782954
coderzry <18348738308@163.com>
2021-10-03 21:28:48 +0800

    extend的使用

---
+++ src/01_mixin和extends/pages/BasePage.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <!-- 这里的不会被继承 只会继承对象中的选项 -->
+    <h2>哈哈哈哈</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      title: 'Hello, Page'
+    }
+  },
+  methods: {
+    bar() {
+      console.log('base page bar');
+    }
+  },
+}
+</script>
+
+<style>
+
+</style>
