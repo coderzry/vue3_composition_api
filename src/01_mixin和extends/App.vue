13919e84579c9ee4fddb4c2ae7d86a7516df4867
coderzry <18348738308@163.com>
2021-10-03 20:47:40 +0800

    mixin的基本使用

---
+++ src/01_mixin和extends/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>{{ message }}</div>
+  <button @click="foo">按钮</button>
+</template>
+
+<script>
+import { demoMixin } from '../01_mixin和extends/mixins/demoMinxin'
+export default {
+  mixins: [demoMixin],
+  data() {
+    return {
+      title: 'Hello World'
+    }
+  },
+  methods: {
+    
+  },
+}
+</script>
+
+<style>
+</style>
+  
