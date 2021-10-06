e6cb34dc1ab36b0c25117e2506ca38571b14b86b
coderzry <18348738308@163.com>
2021-10-06 20:30:07 +0800

    useScrollPosition的封装练习

--- src/07_componsitionAPI练习/App.vue
+++ src/07_componsitionAPI练习/App.vue
@@ -4,12 +4,20 @@
     <h2>doubleCounter: {{doubleCounter}}</h2>
     <button @click="increment">counter+1</button>
     <button @click="decrement">counter-1</button>
+
+    <p class="content"></p>
+
+    <div class="scroll">
+      <div class="scroll-X">scrollX: {{scrollX}}</div>
+      <div class="scroll-Y">scrollY: {{scrollY}}</div>
+    </div>
   </div>
 </template>
 
 <script>
 import useCounter from './hooks/useCounter'
 import useTitle from './hooks/useTitle'
+import useScrollPosition from './hooks/useScrollPosition'
 export default {
   setup() {
     // counter
@@ -22,16 +30,29 @@ export default {
       titleRef.value = 'kobe'
     }, 2000)
 
+    // 滚动位置
+    const { scrollX, scrollY } = useScrollPosition()
+
     return {
       counter,
       doubleCounter,
       increment,
-      decrement
+      decrement,
+      scrollX,
+      scrollY
     }
   }
 }
 </script>
 
-<style>
-
+<style scoped>
+.content {
+  width: 3000px;
+  height: 5000px;
+}
+.scroll {
+  position: fixed;
+  right: 30px;
+  bottom: 30px;
+}
 </style>
