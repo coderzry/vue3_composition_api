2e83ecde5898cafb33096368713045459c521aef
coderzry <18348738308@163.com>
2021-10-08 22:46:44 +0800

    局部自定义指令

---
+++ src/10_自定义指令/02_局部自定义指令.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <input type="text" v-focus>
+  </div>
+</template>
+
+<script>
+import { ref, onMounted } from 'vue'
+export default {
+  // 官方文档： https://v3.cn.vuejs.org/guide/custom-directive.html#%E7%AE%80%E4%BB%8B
+  // 局部指令
+  // directives: {
+  //   focus: {
+  //     mounted(el, bindings, vnode, preVnode) {
+  //       console.log('focus mounted');
+  //       el.focus()
+  //     },
+  //   }
+  // }
+}
+</script>
+
+<style>
+
+</style>
